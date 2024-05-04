6f8bc0453dc95c5b908a5a56964d13001265dd1f
arpitsahuu <arpitsahu358@gmail.com>
2024-05-04 18:11:37 +0530

    change backend url

--- client/axiosconfig.js
+++ client/axiosconfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: "https://api.eagleart.in",
-    baseURL: "http://localhost:4000",
+    baseURL: "https://api.eagleart.in",
+    // baseURL: "http://localhost:4000",
     withCredentials: true,
 });
 
